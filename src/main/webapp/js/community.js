f18817bf96400cb68401a3dd4ab6d4abb6f1918a
waahm7 <waahm7@gmail.com>
2019-05-30 14:23:39 +0500

    Community page bug fix

--- src/main/webapp/js/community.js
+++ src/main/webapp/js/community.js
@@ -1,25 +1,23 @@
-/** Fetches users and adds them to the page. */
-function fetchUserList() {
+function fetchUserList(){
     const url = '/user-list';
-    fetch(url).then((response) = > {
+    fetch(url).then((response) => {
         return response.json();
-    }).
-        then((users) = > {
-            const list = document.getElementById('list');
-        list.innerHTML = '';
+}).then((users) => {
+        const list = document.getElementById('list');
+    list.innerHTML = '';
 
-            users.forEach((user) = > {
-                const userListItem = buildUserListItem(user);
-            list.appendChild(userListItem);
-            });
-    });
+    users.forEach((user) => {
+        const userListItem = buildUserListItem(user);
+    list.appendChild(userListItem);
+});
+});
 }
 
 /**
  * Builds a list element that contains a link to a user page, e.g.
  * <li><a href="/user-page.html?user=test@example.com">test@example.com</a></li>
  */
-function buildUserListItem(user) {
+function buildUserListItem(user){
     const userLink = document.createElement('a');
     userLink.setAttribute('href', '/user-page.html?user=' + user);
     userLink.appendChild(document.createTextNode(user));
@@ -29,6 +27,6 @@ function buildUserListItem(user) {
 }
 
 /** Fetches data and populates the UI of the page. */
-function buildUI() {
+function buildUI(){
     fetchUserList();
 }
