893f803af7c0fa0d62ae99d0a86691df2c0b302a
waahm7 <waahm7@gmail.com>
2019-06-02 01:32:33 +0500

    Fix redundant variable

--- src/main/java/com/google/codeu/servlets/UserListServlet.java
+++ src/main/java/com/google/codeu/servlets/UserListServlet.java
@@ -1,7 +1,5 @@
 package com.google.codeu.servlets;
 
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
 import com.google.codeu.data.Datastore;
 
 import javax.servlet.annotation.WebServlet;
@@ -9,7 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Set;
 
 @WebServlet("/user-list")
 public class UserListServlet extends HttpServlet {
@@ -24,8 +21,7 @@ public class UserListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         
-        Set<String> users=datastore.getUsers();
-        for(String user:users)
+        for(String user:datastore.getUsers())
             response.getOutputStream().println(user);
 
     }
