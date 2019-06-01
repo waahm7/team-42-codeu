51735153ef266fd3c74dadddc1eb590f11ae40a9
waahm7 <waahm7@gmail.com>
2019-06-02 01:39:15 +0500

    redundant variable fix

--- src/main/java/com/google/codeu/servlets/UserListServlet.java
+++ src/main/java/com/google/codeu/servlets/UserListServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Set;
 
 @WebServlet("/user-list")
 public class UserListServlet extends HttpServlet {
@@ -24,10 +23,9 @@ public class UserListServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         response.setContentType("application/json");
-        Set<String> users = datastore.getUsers();
+
         Gson gson = new Gson();
-        String json = gson.toJson(users);
-        response.getOutputStream().println(json);
+        response.getOutputStream().println(gson.toJson(datastore.getUsers()));
 
     }
 }
