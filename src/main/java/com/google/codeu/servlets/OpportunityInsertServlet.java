610b696f20b4848a8127e853c7e35b4c0f0bf8d6
waahm7 <waahm7@gmail.com>
2019-06-15 21:48:18 +0500

    adds opportunity insert class

---
+++ src/main/java/com/google/codeu/servlets/OpportunityInsertServlet.java
@@ -0,0 +1,64 @@
+package com.google.codeu.servlets;
+
+import com.google.codeu.data.Datastore;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Handles fetching and saving user data.
+ */
+@WebServlet("/insertOpportunity")
+
+public class OpportunityInsertServlet extends HttpServlet {
+
+  private Datastore datastore;
+
+  @Override
+  public void init() {
+    datastore = new Datastore();
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    response.setContentType("text/html");
+
+     String id = request.getParameter("id");
+     String minAge= request.getParameter("minAge"),maxAge= request.getParameter("maxAge");
+     String title= request.getParameter("title"), description= request.getParameter("id");
+     String applyLink= request.getParameter("ApplyLink");
+     String advertisementImageUrl= request.getParameter("Image");
+    //Requirements
+     String gender= request.getParameter("Gender"), eductationLevel= request.getParameter("Education");
+     String otherRequirments= request.getParameter("AdditionalRequirements"), additionalLinks= request.getParameter("AdditionalLinks");
+     String dueDate= request.getParameter("DueDate"), startDate= request.getParameter("StartDate");
+     String recurring= request.getParameter("Recurring");
+
+    
+    response.getOutputStream().println(id);
+    response.getOutputStream().println(minAge);
+    response.getOutputStream().println(maxAge);
+    response.getOutputStream().println(title);
+    response.getOutputStream().println(description);
+    response.getOutputStream().println(applyLink);
+    response.getOutputStream().println(advertisementImageUrl);
+    response.getOutputStream().println(eductationLevel);
+    response.getOutputStream().println(otherRequirments);
+    response.getOutputStream().println(gender);
+    response.getOutputStream().println(additionalLinks);
+    response.getOutputStream().println(dueDate);
+    response.getOutputStream().println(startDate);
+    response.getOutputStream().println(recurring);
+
+
+    if(id==null)
+      response.getOutputStream().println("No id given");
+    else
+      response.getOutputStream().println("Opportunity id="+id);
+
+  }
+}
