07196a310586e598a984e0eee7e37e00b08d542d
azanbinzahid <azanbinzahid@gmail.com>
2019-06-22 23:02:40 +0500

    removed non-used lines

--- src/main/java/com/google/codeu/servlets/AllOppurtunitiesGetServlet.java
+++ src/main/java/com/google/codeu/servlets/AllOppurtunitiesGetServlet.java
@@ -7,11 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.api.mail.MailService.Message;
 import com.google.appengine.repackaged.com.google.gson.Gson;
 import com.google.codeu.data.Datastore;
-import com.google.codeu.data.Opportunity;
-import com.google.gson.JsonObject;
 import java.util.List;
 
 
@@ -37,7 +34,6 @@ public class AllOppurtunitiesGetServlet extends HttpServlet {
 
     response.setContentType("application/json");
     Gson gson = new Gson();
-    JsonObject jsonObject = new JsonObject();
     String res = "["; 
 
 
@@ -45,14 +41,10 @@ public class AllOppurtunitiesGetServlet extends HttpServlet {
     List <com.google.codeu.data.Opportunity> allOpp = datastore.getAllOpportunities();
     for (com.google.codeu.data.Opportunity tempOpportunity : allOpp) {
       String jsonInString = gson.toJson(tempOpportunity);
-      // String id = Long.toString(tempOpportunity.getId());
-      // jsonObject.add("Oppurtunities", jsonInString);  
       res = res + jsonInString + ",";
-      
     }
     res = res.substring(0, res.length()-1);
     res = res + "]";
-    // response.getOutputStream().println(jsonObject.toString());
     response.getWriter().write(res);
   }
 }
