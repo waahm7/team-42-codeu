0efe47dbfa2fd5f20b38dba328805e0780932ad4
azanbinzahid <azanbinzahid@gmail.com>
2019-06-23 20:55:32 +0500

    added new api key

--- src/main/java/com/google/codeu/servlets/StatsPageServlet.java
+++ src/main/java/com/google/codeu/servlets/StatsPageServlet.java
@@ -35,13 +35,15 @@ public class StatsPageServlet extends HttpServlet{
 
     int messageCount = datastore.getTotalMessageCount();
     int userCount = datastore.getTotalUserCount();
-    com.google.codeu.data.Message LongestMessage = datastore.LongestMessage();
+    // com.google.codeu.data.Message LongestMessage = datastore.LongestMessage();
 
     JsonObject jsonObject = new JsonObject();
     jsonObject.addProperty("userCount", userCount);
     jsonObject.addProperty("messageCount", messageCount);
-    jsonObject.addProperty("LongestMessage", LongestMessage.getText());
-    jsonObject.addProperty("LongestMessageLenght", LongestMessage.getText().length());
+    // jsonObject.addProperty("LongestMessage", LongestMessage.getText());
+    // jsonObject.addProperty("LongestMessageLenght", LongestMessage.getText().length());
+    jsonObject.addProperty("Empty", 0);
+
     response.getOutputStream().println(jsonObject.toString());
   }
 }
