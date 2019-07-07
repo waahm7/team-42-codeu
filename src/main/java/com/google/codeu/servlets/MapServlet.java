91460a6daa2b8ec22335bd3cfa1d1692ea60ff40
azanbinzahid <azanbinzahid@gmail.com>
2019-07-07 20:19:51 +0500

    changed import settings in api

--- src/main/java/com/google/codeu/servlets/MapServlet.java
+++ src/main/java/com/google/codeu/servlets/MapServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.repackaged.com.google.gson.Gson;
+import com.google.gson.Gson;
 import com.google.codeu.data.Datastore;
 import java.util.List;
 import java.util.Date;
